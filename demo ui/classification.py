d44cdf3357f77c9fa4c5935810ead11734e970fb
chiseng <chiseng_wong@mymail.sutd.edu.sg>
2020-07-21 14:21:59 +0800

    fixed rfid

--- demo ui/classification.py
+++ demo ui/classification.py
@@ -5,7 +5,7 @@ from joblib import dump, load
 
 filename = r'D:\Capstone\CODE\Capstone-system-integration\demo ui\data\single_sample_feature.csv'
 
-def classfication():
+def classification():
     df: pandas.DataFrame = pandas.read_csv(filename)
     columns = ["Size_L35", "kl_L35", "L35.Mean", "MeanSize_35", "combo3" , "Size_InvL35", "MeanSize_L", "InvL35.Mean", "combo5", "L35.Skewness", "L35.Kurt", "combo8", "combo10", "MeanSize_InvL", "combo9", "combo4", "L350.WBC.s", "combo6", "kl_invL35", "L35.WBC.s"]
     selected_features = df[columns].to_numpy()
